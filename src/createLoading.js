8db3a994bcce80ceadef88ce9a8ddace57ad0e10
zeraye <zeraye@protonmail.com>
2022-11-11 21:23:04 +0100

    fix: change loadingDiv from const to global
    Such change was necessary, becase in the future createLoading.js will be used not only in content_scripts, but also in the background script.

--- src/createLoading.js
+++ src/createLoading.js
@@ -1,4 +1,16 @@
-const loading = document.createElement("div");
-loading.textContent = "Analysing chess game...";
-loading.setAttribute("class", "caal-loading");
-document.body.appendChild(loading);
+/*
+ * loadingDiv is a global variable, because I wasn't able
+ * to check if typeof loadingDiv === "undefined".
+ * Normally typeof don't need variable to be initialized,
+ * but for some reason I got an error that there is
+ * no such variable as loadingDiv.
+ * Firefox version: 106.0.2 (64-bit) on Ubuntu 20.04.1
+ */
+loadingDiv = document.createElement("div");
+loadingDiv.textContent = "🔍 Analysing game...";
+loadingDiv.setAttribute("class", "cal-loading");
+document.body.appendChild(loadingDiv);
+
+/* Why this line is necessary? */
+/* https://stackoverflow.com/a/44774834 */
+undefined;
