f9ba3e819803a215d60ee3ad9d1d4163c2dd3641
zeraye <zeraye@protonmail.com>
2023-06-30 11:09:02 +0200

    refactor: remove comment describing removed code

--- src/js/createLoading.js
+++ src/js/createLoading.js
@@ -1,11 +1,3 @@
-/*
- * loadingDiv is a global variable, because I wasn't able
- * to check if typeof loadingDiv === "undefined".
- * Normally typeof don't need variable to be initialized,
- * but for some reason I got an error that there is
- * no such variable as loadingDiv.
- * Firefox version: 106.0.2 (64-bit) on Ubuntu 20.04.1
- */
 (() => {
   const loadingDiv = document.createElement("div");
   loadingDiv.textContent = "🔍 Analysing game...";
