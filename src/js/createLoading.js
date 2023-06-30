f08987ad80cdb19e195df12162bb4ca72f525e19
zeraye <zeraye@protonmail.com>
2023-06-30 10:45:58 +0200

    refactor: avoid polluting global namespace
    Use an IIFE to avoid polluting global namespace.

--- src/js/createLoading.js
+++ src/js/createLoading.js
@@ -6,10 +6,12 @@
  * no such variable as loadingDiv.
  * Firefox version: 106.0.2 (64-bit) on Ubuntu 20.04.1
  */
-loadingDiv = document.createElement("div");
-loadingDiv.textContent = "🔍 Analysing game...";
-loadingDiv.setAttribute("class", "cal-loading");
-document.body.appendChild(loadingDiv);
+(() => {
+  const loadingDiv = document.createElement("div");
+  loadingDiv.textContent = "🔍 Analysing game...";
+  loadingDiv.setAttribute("class", "cal-loading");
+  document.body.appendChild(loadingDiv);
+})();
 
 /* Why this line is necessary? */
 /* https://stackoverflow.com/a/44774834 */
