df5710b7a20130b32202fc7e4434981b67ea6d59
zeraye <zeraye@protonmail.com>
2024-07-24 20:54:01 +0200

    fix: remove necessary line

--- src/js/createLoading.js
+++ src/js/createLoading.js
@@ -4,7 +4,3 @@
   loadingDiv.setAttribute("class", "cal-loading");
   document.body.appendChild(loadingDiv);
 })();
-
-/* Why this line is necessary? */
-/* https://stackoverflow.com/a/44774834 */
-undefined;
